cbffd17e05df544ee91ab3a845fb4e5e7341bf60
Rk <r.kprajapati215@gmail.com>
2017-10-02 16:03:22 +0530

    updated code

--- batterypercentage code file/MainActivity.java
+++ batterypercentage code file/MainActivity.java
@@ -3,28 +3,40 @@ package com.example.r_k.intentservicebatterypercentage;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.Toolbar;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity{
-    private myservice service;
+    myservice service;
     Switch Batteryswitch;
     Intent intent;
     NotificationCompat.Builder notificationCompat;
     NotificationManager manager;
 
+    Toolbar toolbar;
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //add toolbar and find id.
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        //set toolbar title as our app name.
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        //set toolbar title text color as 'white'.
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+        //find switch(android view/control) id.
         Batteryswitch = (Switch) findViewById(R.id.Batteryswitch);
-
+        //check the current switch state (enable/disable).
         preferences = getSharedPreferences("myPreference", MODE_PRIVATE);
+        //set the sharedPreference current state to switch. by default must be false.because switch by default is turn off you have to turn on.
         Batteryswitch.setChecked
                 (preferences.getBoolean("Switch", false));
 
@@ -32,30 +44,33 @@ public class MainActivity extends AppCompatActivity{
         Batteryswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
+                //check if the switch is checked or not.
                 if (Batteryswitch.isChecked() == true) {
-                    if (service == null) {
+                    //switch is checked.
+                    //check service is null or not.
+                    if (service==null) {
+                        //start the service here
                         intent = new Intent(MainActivity.this, myservice.class);
+                        //set the current switch state to sharedPreference.
+                        preferences = getSharedPreferences("myPreference", MODE_PRIVATE);
+                        editor = preferences.edit();
 
-     preferences = getSharedPreferences("myPreference", MODE_PRIVATE);
+                        editor.putBoolean("Switch", true);
 
-     editor = preferences.edit();
-
-     editor.putBoolean("Switch", true);
-
-     editor.commit();
+                        editor.commit();
                         startService(intent);
                     }
                 } else {
+                    //means service running so stop all over features.
+                    //set the current switch state to sharedPreference.
                     editor = preferences.edit();
                     editor.putBoolean("Switch", false);
                     editor.apply();
-                    intent = new Intent(MainActivity.this, myservice.class);
 
+                    intent = new Intent(MainActivity.this, myservice.class);
                     stopService(intent);//stop service.
 
                     //cancel notification from status bar .
-
                     notificationCompat = (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
                             .setContentTitle("Battery is low")
                             .setContentText("Current percentage is ")
